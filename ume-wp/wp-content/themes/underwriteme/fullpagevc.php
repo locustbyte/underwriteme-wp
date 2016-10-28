689451f002b4648af0c689a2831691438442e375
Tan Kent <tan.kent01@space01.co.uk>
2016-10-28 04:31:46 +0100

    update

--- ume-wp/wp-content/themes/underwriteme/fullpagevc.php
+++ ume-wp/wp-content/themes/underwriteme/fullpagevc.php
@@ -17,6 +17,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="https://daneden.github.io/animate.css/animate.min.css">
+<script src="wp-content/themes/underwriteme/js/ume-animations.min.js"></script>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
@@ -56,8 +58,10 @@
 <div class="ume-logo">
   <img src="images/ume-logo.png" />
 </div>
+
+<!-- <div class="blk-background"></div> -->
 <div class="ume-background">
-  <svg width="1000" height="1000"></svg>
+  <canvas></canvas>
 </div>
 <div id="fullpage_wrapper">
 <?php
@@ -77,147 +81,363 @@ while ( have_posts() )
   ob_end_clean();
   echo MCW_FullPage_Template_Lib::removeJS($mcw_fp_t_output);
 ?>
+<script>
 
-<script type="text/javascript" charset="utf-8">
+  /**
+ * @author mrdoob / http://mrdoob.com/
+ */
 
-      var w = 960, h = 500;
+var Stats = function () {
 
-      var labelDistance = 0;
+  var startTime = Date.now(), prevTime = startTime;
+  var ms = 0, msMin = Infinity, msMax = 0;
+  var fps = 0, fpsMin = Infinity, fpsMax = 0;
+  var frames = 0, mode = 0;
 
-      var vis = d3.select("svg").attr("width", $(window).width()).attr("height", $(window).height());
-      var force;
-      var nodes = [];
-      var labelAnchors = [];
-      var labelAnchorLinks = [];
-      var links = [];
-      var node;
+  var container = document.createElement( 'div' );
+  container.id = 'stats';
+  container.addEventListener( 'mousedown', function ( event ) { event.preventDefault(); setMode( ++ mode % 2 ) }, false );
+  container.style.cssText = 'width:80px;opacity:0.9;cursor:pointer';
 
-function startnew(){
-  $('svg').empty()
-  nodes = [];
-  labelAnchors = [];
-  labelAnchorLinks = [];
-  links = [];
+  var fpsDiv = document.createElement( 'div' );
+  fpsDiv.id = 'fps';
+  fpsDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#002';
+  container.appendChild( fpsDiv );
 
-      for(var i = 0; i < 30; i++) {
-        node = {
-          label : ""
-        };
-        nodes.push(node);
-        labelAnchors.push({
-          node : node
-        });
-        labelAnchors.push({
-          node : node
-        });
-      };
+  var fpsText = document.createElement( 'div' );
+  fpsText.id = 'fpsText';
+  fpsText.style.cssText = 'color:#0ff;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
+  fpsText.innerHTML = 'FPS';
+  fpsDiv.appendChild( fpsText );
 
-      for(var i = 0; i < nodes.length; i++) {
-        for(var j = 0; j < i; j++) {
-          if(Math.random() > .95)
-            links.push({
-              source : i,
-              target : j,
-              weight : Math.random()
-            });
-        }
-        labelAnchorLinks.push({
-          source : i * 2,
-          target : i * 2 + 1,
-          weight : 1
-        });
-      };
+  var fpsGraph = document.createElement( 'div' );
+  fpsGraph.id = 'fpsGraph';
+  fpsGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0ff';
+  fpsDiv.appendChild( fpsGraph );
 
-      var rndtan = Math.floor(Math.random() * -3000) + -2000  
-      console.log(rndtan)
+  while ( fpsGraph.children.length < 74 ) {
 
-      force = d3.layout.force().size([w, h]).nodes(nodes).links(links).gravity(1).linkDistance(50).charge(-3000).linkStrength(function(x) {
-        return x.weight * 10
-      });
+    var bar = document.createElement( 'span' );
+    bar.style.cssText = 'width:1px;height:30px;float:left;background-color:#113';
+    fpsGraph.appendChild( bar );
 
+  }
 
-      force.start();
+  var msDiv = document.createElement( 'div' );
+  msDiv.id = 'ms';
+  msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;display:none';
+  container.appendChild( msDiv );
 
-      var force2 = d3.layout.force().nodes(labelAnchors).links(labelAnchorLinks).gravity(0).linkDistance(0).linkStrength(8).charge(-100).size([w, h]);
-      force2.start();
+  var msText = document.createElement( 'div' );
+  msText.id = 'msText';
+  msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
+  msText.innerHTML = 'MS';
+  msDiv.appendChild( msText );
 
-      var link = vis.selectAll("line.link").data(links).enter().append("svg:line").attr("class", "link").style("stroke", "#272E34");
+  var msGraph = document.createElement( 'div' );
+  msGraph.id = 'msGraph';
+  msGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0f0';
+  msDiv.appendChild( msGraph );
 
-      var node = vis.selectAll("g.node").data(force.nodes()).enter().append("svg:g").attr("class", "node");
-      node.append("svg:circle").attr("r", 5).style("fill", "#272E34").style("stroke", "#272E34").style("stroke-width", 3);
-      node.call(force.drag);
+  while ( msGraph.children.length < 74 ) {
 
+    var bar = document.createElement( 'span' );
+    bar.style.cssText = 'width:1px;height:30px;float:left;background-color:#131';
+    msGraph.appendChild( bar );
 
-      var anchorLink = vis.selectAll("line.anchorLink").data(labelAnchorLinks)//.enter().append("svg:line").attr("class", "anchorLink").style("stroke", "#999");
+  }
 
-      var anchorNode = vis.selectAll("g.anchorNode").data(force2.nodes()).enter().append("svg:g").attr("class", "anchorNode");
-      anchorNode.append("svg:circle").attr("r", 0).style("fill", "#272E34");
-        anchorNode.append("svg:text").text(function(d, i) {
-        return i % 2 == 0 ? "" : d.node.label
-      }).style("fill", "#555").style("font-family", "Arial").style("font-size", 12);
+  var setMode = function ( value ) {
 
-      var updateLink = function() {
-        this.attr("x1", function(d) {
-          return d.source.x;
-        }).attr("y1", function(d) {
-          return d.source.y;
-        }).attr("x2", function(d) {
-          return d.target.x;
-        }).attr("y2", function(d) {
-          return d.target.y;
-        });
+    mode = value;
+
+    switch ( mode ) {
+
+      case 0:
+        fpsDiv.style.display = 'block';
+        msDiv.style.display = 'none';
+        break;
+      case 1:
+        fpsDiv.style.display = 'none';
+        msDiv.style.display = 'block';
+        break;
+    }
+
+  }
+
+  var updateGraph = function ( dom, value ) {
+
+    var child = dom.appendChild( dom.firstChild );
+    child.style.height = value + 'px';
+
+  }
+
+  return {
+
+    REVISION: 11,
+
+    domElement: container,
+
+    setMode: setMode,
+
+    begin: function () {
+
+      startTime = Date.now();
+
+    },
+
+    end: function () {
+
+      var time = Date.now();
+
+      ms = time - startTime;
+      msMin = Math.min( msMin, ms );
+      msMax = Math.max( msMax, ms );
+
+      msText.textContent = ms + ' MS (' + msMin + '-' + msMax + ')';
+      updateGraph( msGraph, Math.min( 30, 30 - ( ms / 200 ) * 30 ) );
+
+      frames ++;
+
+      if ( time > prevTime + 1000 ) {
+
+        fps = Math.round( ( frames * 1000 ) / ( time - prevTime ) );
+        fpsMin = Math.min( fpsMin, fps );
+        fpsMax = Math.max( fpsMax, fps );
+
+        fpsText.textContent = fps + ' FPS (' + fpsMin + '-' + fpsMax + ')';
+        updateGraph( fpsGraph, Math.min( 30, 30 - ( fps / 100 ) * 30 ) );
+
+        prevTime = time;
+        frames = 0;
 
       }
 
-      var updateNode = function() {
-        this.attr("transform", function(d) {
-          return "translate(" + d.x + "," + d.y + ")";
-        });
+      return time;
 
-      }
+    },
 
+    update: function () {
 
-      force.on("tick", function() {
+      startTime = this.end();
 
-        force2.start();
+    }
 
-        node.call(updateNode);
+  }
 
-        anchorNode.each(function(d, i) {
-          if(i % 2 == 0) {
-            d.x = d.node.x;
-            d.y = d.node.y;
-          } else {
-            var b = this.childNodes[1].getBBox();
-
-            var diffX = d.x - d.node.x;
-            var diffY = d.y - d.node.y;
-
-            var dist = Math.sqrt(diffX * diffX + diffY * diffY);
-
-            var shiftX = b.width * (diffX - dist) / (dist * 2);
-            shiftX = Math.max(-b.width, Math.min(0, shiftX));
-            var shiftY = 5;
-            this.childNodes[1].setAttribute("transform", "translate(" + shiftX + "," + shiftY + ")");
-          }
-        });
-
-
-        anchorNode.call(updateNode);
-
-        link.call(updateLink);
-        anchorLink.call(updateLink);
-
-      });
+};
+    /*
+ * requestAnimationFrame pollyfill
+ */
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
+    return window.setTimeout(callback, 1000 / 60);
+  });
 }
 
-  startnew()
-      
+// Init Stats
+var stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+//document.body.appendChild(stats.domElement);
 
-      
 
-     
+/*!
+ * Mantis.js / jQuery / Zepto.js plugin for Constellation
+ * @version 1.2.2
+ * @author Acauã Montiel <contato@acauamontiel.com.br>
+ * @license http://acaua.mit-license.org/
+ */
+(function ($, window) {
+  /**
+   * Makes a nice constellation on canvas
+   * @constructor Constellation
+   */
+  function Constellation (canvas, options) {
+    var $canvas = $(canvas),
+      context = canvas.getContext('2d'),
+      defaults = {
+        star: {
+          color: 'rgb(45, 55, 62)',
+          width: 1,
+          randomWidth: true
+        },
+        line: {
+          color: 'rgba(255, 255, 255, .5)',
+          width: 1
+        },
+        position: {
+          x: 0, // This value will be overwritten at startup
+          y: 0 // This value will be overwritten at startup
+        },
+        width: window.innerWidth,
+        height: window.innerHeight,
+        velocity: 0.1,
+        length: 70,
+        distance: 150,
+        radius: 150,
+        stars: []
+      },
+      config = $.extend(true, {}, defaults, options);
+      console.log(config)
 
-    </script>
+    function Star () {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+
+      this.vx = (config.velocity - (Math.random() * 0.3));
+      this.vy = (config.velocity - (Math.random() * 0.3));
+
+      this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
+    }
+
+    Star.prototype = {
+      create: function(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 6, false);
+        context.fill();
+      },
+
+      animate: function(){
+        var i;
+        for (i = 0; i < config.length; i++) {
+
+          var star = config.stars[i];
+
+          if (star.y < 0 || star.y > canvas.height) {
+            star.vx = star.vx;
+            star.vy = - star.vy;
+          } else if (star.x < 0 || star.x > canvas.width) {
+            star.vx = - star.vx;
+            star.vy = star.vy;
+          }
+
+          star.x += star.vx;
+          star.y += star.vy;
+        }
+      },
+
+      line: function(){
+        var length = config.length,
+          iStar,
+          jStar,
+          i,
+          j;
+
+        for (i = 0; i < length; i++) {
+          for (j = 0; j < length; j++) {
+            iStar = config.stars[i];
+            jStar = config.stars[j];
+
+            if (
+              (iStar.x - jStar.x) < config.distance &&
+              (iStar.y - jStar.y) < config.distance &&
+              (iStar.x - jStar.x) > - config.distance &&
+              (iStar.y - jStar.y) > - config.distance
+            ) {
+              if (
+                (iStar.x - config.position.x) < config.radius &&
+                (iStar.y - config.position.y) < config.radius &&
+                (iStar.x - config.position.x) > - config.radius &&
+                (iStar.y - config.position.y) > - config.radius
+              ) {
+                context.beginPath();
+                context.moveTo(iStar.x, iStar.y);
+                context.lineTo(jStar.x, jStar.y);
+                context.stroke();
+                context.closePath();
+              }
+            }
+          }
+        }
+      }
+    };
+
+    this.createStars = function () {
+      var length = config.length,
+        star,
+        i;
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      for (i = 0; i < length; i++) {
+        config.stars.push(new Star());
+        star = config.stars[i];
+        
+
+        star.create();
+      }
+
+      star.line();
+      star.animate();
+    };
+
+    this.setCanvas = function () {
+      canvas.width = config.width;
+      canvas.height = config.height;
+    };
+
+    this.setContext = function () {
+      context.fillStyle = config.star.color;
+      context.strokeStyle = config.line.color;
+      context.lineWidth = config.line.width;
+    };
+
+    this.setInitialPosition = function () {
+      if (!options || !options.hasOwnProperty('position')) {
+        config.position = {
+          x: canvas.width * 0.5,
+          y: canvas.height * 0.5
+        };
+      }
+    };
+
+    this.loop = function (callback) {
+      callback();
+
+      window.requestAnimationFrame(function () {
+        stats.begin(); // Only for Stats
+        this.loop(callback);
+        stats.end(); // Only for Stats
+      }.bind(this));
+    };
+
+    this.bind = function () {
+      $canvas.on('mousemove', function(e){
+        config.position.x = e.pageX - $canvas.offset().left;
+        config.position.y = e.pageY - $canvas.offset().top;
+      });
+    };
+
+    this.init = function () {
+      this.setCanvas();
+      this.setContext();
+      this.setInitialPosition();
+      this.loop(this.createStars);
+      this.bind();
+    };
+  }
+
+  $.fn.constellation = function (options) {
+    return this.each(function () {
+      var c = new Constellation(this, options);
+      c.init();
+    });
+  };
+})($, window);
+
+// Init plugin
+$('canvas').constellation({
+  star: {
+    width: 18
+  },
+  line: {
+    color: 'rgb(45, 55,62)'
+  },
+  radius: 1200
+});
+  </script>
 </body>
 </html>
