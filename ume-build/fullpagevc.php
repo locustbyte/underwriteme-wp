01ad3ffd9cff78e8ce7c4e815d38ef00d433ba8f
Tan Kent <tan.kent01@space01.co.uk>
2016-11-03 11:16:47 +0000

    final

--- ume-build/fullpagevc.php
+++ ume-build/fullpagevc.php
@@ -61,7 +61,7 @@
 
 <!-- <div class="blk-background"></div> -->
 <div class="ume-background">
-  <canvas></canvas>
+  <canvas id="canvas">  </canvas>  
 </div>
 <div id="fullpage_wrapper">
 <?php
@@ -81,152 +81,8 @@ while ( have_posts() )
   ob_end_clean();
   echo MCW_FullPage_Template_Lib::removeJS($mcw_fp_t_output);
 ?>
-<script>
-
-  /**
- * @author mrdoob / http://mrdoob.com/
- */
-
-var Stats = function () {
-
-  var startTime = Date.now(), prevTime = startTime;
-  var ms = 0, msMin = Infinity, msMax = 0;
-  var fps = 0, fpsMin = Infinity, fpsMax = 0;
-  var frames = 0, mode = 0;
-
-  var container = document.createElement( 'div' );
-  container.id = 'stats';
-  container.addEventListener( 'mousedown', function ( event ) { event.preventDefault(); setMode( ++ mode % 2 ) }, false );
-  container.style.cssText = 'width:80px;opacity:0.9;cursor:pointer';
-
-  var fpsDiv = document.createElement( 'div' );
-  fpsDiv.id = 'fps';
-  fpsDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#002';
-  container.appendChild( fpsDiv );
-
-  var fpsText = document.createElement( 'div' );
-  fpsText.id = 'fpsText';
-  fpsText.style.cssText = 'color:#0ff;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-  fpsText.innerHTML = 'FPS';
-  fpsDiv.appendChild( fpsText );
-
-  var fpsGraph = document.createElement( 'div' );
-  fpsGraph.id = 'fpsGraph';
-  fpsGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0ff';
-  fpsDiv.appendChild( fpsGraph );
-
-  while ( fpsGraph.children.length < 74 ) {
-
-    var bar = document.createElement( 'span' );
-    bar.style.cssText = 'width:1px;height:30px;float:left;background-color:#113';
-    fpsGraph.appendChild( bar );
-
-  }
-
-  var msDiv = document.createElement( 'div' );
-  msDiv.id = 'ms';
-  msDiv.style.cssText = 'padding:0 0 3px 3px;text-align:left;background-color:#020;display:none';
-  container.appendChild( msDiv );
-
-  var msText = document.createElement( 'div' );
-  msText.id = 'msText';
-  msText.style.cssText = 'color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px';
-  msText.innerHTML = 'MS';
-  msDiv.appendChild( msText );
-
-  var msGraph = document.createElement( 'div' );
-  msGraph.id = 'msGraph';
-  msGraph.style.cssText = 'position:relative;width:74px;height:30px;background-color:#0f0';
-  msDiv.appendChild( msGraph );
-
-  while ( msGraph.children.length < 74 ) {
-
-    var bar = document.createElement( 'span' );
-    bar.style.cssText = 'width:1px;height:30px;float:left;background-color:#131';
-    msGraph.appendChild( bar );
-
-  }
-
-  var setMode = function ( value ) {
-
-    mode = value;
-
-    switch ( mode ) {
-
-      case 0:
-        fpsDiv.style.display = 'block';
-        msDiv.style.display = 'none';
-        break;
-      case 1:
-        fpsDiv.style.display = 'none';
-        msDiv.style.display = 'block';
-        break;
-    }
-
-  }
-
-  var updateGraph = function ( dom, value ) {
-
-    var child = dom.appendChild( dom.firstChild );
-    child.style.height = value + 'px';
-
-  }
-
-  return {
-
-    REVISION: 11,
-
-    domElement: container,
-
-    setMode: setMode,
-
-    begin: function () {
-
-      startTime = Date.now();
-
-    },
-
-    end: function () {
-
-      var time = Date.now();
-
-      ms = time - startTime;
-      msMin = Math.min( msMin, ms );
-      msMax = Math.max( msMax, ms );
-
-      msText.textContent = ms + ' MS (' + msMin + '-' + msMax + ')';
-      updateGraph( msGraph, Math.min( 30, 30 - ( ms / 200 ) * 30 ) );
-
-      frames ++;
-
-      if ( time > prevTime + 1000 ) {
-
-        fps = Math.round( ( frames * 1000 ) / ( time - prevTime ) );
-        fpsMin = Math.min( fpsMin, fps );
-        fpsMax = Math.max( fpsMax, fps );
-
-        fpsText.textContent = fps + ' FPS (' + fpsMin + '-' + fpsMax + ')';
-        updateGraph( fpsGraph, Math.min( 30, 30 - ( fps / 100 ) * 30 ) );
-
-        prevTime = time;
-        frames = 0;
-
-      }
-
-      return time;
-
-    },
-
-    update: function () {
-
-      startTime = this.end();
-
-    }
-
-  }
-
-};
-    /*
+<script> 
+  /*
  * requestAnimationFrame pollyfill
  */
 if (!window.requestAnimationFrame) {
@@ -236,12 +92,12 @@ if (!window.requestAnimationFrame) {
 }
 
 // Init Stats
-var stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-//document.body.appendChild(stats.domElement);
+// var stats = new Stats();
+// stats.setMode(0);
+// stats.domElement.style.position = 'absolute';
+// stats.domElement.style.left = '0px';
+// stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
 
 
 /*!
@@ -250,6 +106,9 @@ stats.domElement.style.top = '0px';
  * @author Acauã Montiel <contato@acauamontiel.com.br>
  * @license http://acaua.mit-license.org/
  */
+ function randomStarColor(){
+      return Math.round(Math.random()*1) + 1
+    }
 (function ($, window) {
   /**
    * Makes a nice constellation on canvas
@@ -260,13 +119,13 @@ stats.domElement.style.top = '0px';
       context = canvas.getContext('2d'),
       defaults = {
         star: {
-          color: 'rgb(45, 55, 62)',
+          color: '#2a333a',
           width: 1,
           randomWidth: true
         },
         line: {
-          color: 'rgba(255, 255, 255, .5)',
-          width: 1
+          color: 'rgba(0, 0, 0, 1)',
+          width: 0.2
         },
         position: {
           x: 0, // This value will be overwritten at startup
@@ -275,20 +134,22 @@ stats.domElement.style.top = '0px';
         width: window.innerWidth,
         height: window.innerHeight,
         velocity: 0.1,
-        length: 70,
-        distance: 150,
+        length: 250,
+        distance: 120,
         radius: 150,
         stars: []
       },
       config = $.extend(true, {}, defaults, options);
-      console.log(config)
+
+
+    
 
     function Star () {
       this.x = Math.random() * canvas.width;
       this.y = Math.random() * canvas.height;
 
-      this.vx = (config.velocity - (Math.random() * 0.3));
-      this.vy = (config.velocity - (Math.random() * 0.3));
+      this.vx = (config.velocity - (Math.random() * 0.5));
+      this.vy = (config.velocity - (Math.random() * 0.5));
 
       this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
     }
@@ -296,7 +157,7 @@ stats.domElement.style.top = '0px';
     Star.prototype = {
       create: function(){
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 6, false);
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         context.fill();
       },
 
@@ -365,7 +226,6 @@ stats.domElement.style.top = '0px';
       for (i = 0; i < length; i++) {
         config.stars.push(new Star());
         star = config.stars[i];
-        
 
         star.create();
       }
@@ -380,6 +240,8 @@ stats.domElement.style.top = '0px';
     };
 
     this.setContext = function () {
+
+      
       context.fillStyle = config.star.color;
       context.strokeStyle = config.line.color;
       context.lineWidth = config.line.width;
@@ -398,14 +260,14 @@ stats.domElement.style.top = '0px';
       callback();
 
       window.requestAnimationFrame(function () {
-        stats.begin(); // Only for Stats
+        //stats.begin(); // Only for Stats
         this.loop(callback);
-        stats.end(); // Only for Stats
+        //stats.end(); // Only for Stats
       }.bind(this));
     };
 
     this.bind = function () {
-      $canvas.on('mousemove', function(e){
+      $(window).on('mousemove', function(e){
         config.position.x = e.pageX - $canvas.offset().left;
         config.position.y = e.pageY - $canvas.offset().top;
       });
@@ -431,13 +293,15 @@ stats.domElement.style.top = '0px';
 // Init plugin
 $('canvas').constellation({
   star: {
-    width: 18
+    width: 3
   },
   line: {
-    color: 'rgb(45, 55,62)'
+    color: '#2a333a'
   },
-  radius: 1200
+  radius: 250
 });
-  </script>
+
+
+</script>
 </body>
 </html>
--- ume-build/js/ume-animations.js
+++ ume-build/js/ume-animations.js
@@ -7,3 +7,22 @@ function componentsSlideOne(){
 
 	setTimeout(function(){ $(".blk-background").addClass('fadeOut') }, 2000);
 }
+
+function componentsSlideOne(){
+	console.log("was doing")
+	setTimeout(function(){ $(".component-slide-0-pwr-btn").addClass('zoomIn').removeClass('zoomOut hide') }, 500);
+	setTimeout(function(){ $(".component-slide-0-title").addClass('zoomIn').removeClass('zoomOut hide') }, 1000);
+
+	setTimeout(function(){ $(".scroll-down-mouse-icon").addClass('bottom-up').removeClass('bottom-down hide') }, 1300);
+
+	setTimeout(function(){ $(".blk-background").addClass('fadeOut') }, 2000);
+}
+
+function comparisonSlideOne(){
+	setTimeout(function(){ $(".comparison-slide-0-btn").addClass('zoomIn').removeClass('zoomOut hide') }, 500);
+	setTimeout(function(){ $(".component-slide-0-title").addClass('zoomIn').removeClass('zoomOut hide') }, 1000);
+
+	setTimeout(function(){ $(".scroll-down-mouse-icon").addClass('bottom-up').removeClass('bottom-down hide') }, 1300);
+
+	setTimeout(function(){ $(".blk-background").addClass('fadeOut') }, 2000);	
+}
